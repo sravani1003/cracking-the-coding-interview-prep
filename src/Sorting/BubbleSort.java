b49d96282d2ac42aa51d12ad3963b548793c27c6
sravani1003 <52522671+sravani1003@users.noreply.github.com>
2020-05-19 13:59:38 -0400

    commit_18_05

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+public class BubbleSort {
+    public int[] bubbleSort(int[] array){
+        for(int i = 0; i < array.length; i++){
+            for(int j = i + 1; j < array.length; j++){
+                if(array[j] < array[i]){
+                    int temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {3,1,7,2,6,4,9,8};
+        BubbleSort obj = new BubbleSort();
+        obj.bubbleSort(array);
+        for(int i = 0 ; i < array.length; i++){
+            System.out.print( array[i] + " ");
+        }
+        System.out.println();
+    }
+}
